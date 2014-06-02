ca078db297cf2f4d05e3ee7cf74dda3730c1c982
logan99 <nitish.courses@gmail.com>
2014-06-02 17:42:22 +0000

    more is better

--- index.php
+++ index.php
@@ -27,8 +27,58 @@
       $Val=101; // you forgot the semicolon here, statements are terminated using semicolon as we are used to do in C. But in Python semicolon is optional
           echo "Value stored is " . $Val;
           
+          /*
+          //invalid variable reference
+          echo "Use the value " .$val;
+          */
+
+          // Now lets print something on console..
+          print "<br/>This is print function";
+          echo "<br/>This is echo";
+          print "<br/>both echo and print works";
+          echo "<br/>but echo is faster than print, so always remember to use echo.";
+          print "<br/>oh you semi-colon darling :D";
       ?>
     
+    <h2> Strings </h2>
+    <!-- This is an HTML comment, when working outside the php tag, use this way of commenting stuff..-->
+    <!-- Please insert a php tag below, so that we can write php code there, prerna-->
+    <?php
+      // good now let's use string, string is anything that is inside colon 'this is a string' or "this is a stirng"
+      // String variables also start with $ symbols (dollar) all variables start with $ symbol..
+      $nitish = "nitish";
+      $prerna = "prerna";
+      echo "<br/>" .  $nitish . " loves " . $prerna;
+      // now type a sentence that says prerna loves nitish below
+      echo "<br/>" . $prerna . " also loves " . $nitish;
+    ?>
+    <h2> Numbers </h2>
+     <?php
+      $x = 5985;
+      var_dump($x);
+      echo "<br>"; 
+      $x = -345; // negative number 
+      var_dump($x);
+      echo "<br>"; 
+      $x = 0x8C; // hexadecimal number
+      var_dump($x);
+      echo "<br>";
+      $x = 047; // octal number
+      var_dump($x);
+      //var_dump function dumps all the infromation about the variable or object passed though the parameter.
+      //here we are storing the no and dumping it whats the use
+      // please read the documentation, http://www.php.net/manual/en/function.var-dump.php
+      // sometimes while coding, we may want to know about the type or structure of the object in use. So that we can process it in some other part.
+      // until and unless we don't know if it is an integer, or a string or array, we would not be sure of what operations it allows us to do. 
+      // lets try var_dump on array.
+      echo "<br/>";
+      $array = array(1, 2, array("a", "b", "c")); // I initialized array as we do in python LOL u did then u r LOL
+      var_dump($array);
+      // Did you get what var_dump does??
+      //If u will let me read that then only i will go through na
+      // ok read, var_dump is a good function to debug programs.
+      //when done ping me.
 
+      ?>
     </body>
 </html>
