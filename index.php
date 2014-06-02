46bb7ec5e4de80ede0732ee0dbfebcb4e6f1891f
logan99 <nitish.courses@gmail.com>
2014-06-02 16:22:47 +0000

    more

--- index.php
+++ index.php
@@ -5,8 +5,30 @@
   
   <body>
     <?php
+    # this is a comment
+    /* This is multiple line comment like in c
+    also // and # are used for single line comment */
     echo "Hello prerna";
     echo "<br/> Let's collobrate and build something nice!";
+
+    /*
+    Case Sensitivity does not work
+    Echo "this will not work";
+    ecHO "this will also not work"
+    */
 ?>
+    <h5> Variable case sensitivity</h5>
+
+      <?php
+       /* prerna type below an example of bad case sensitive variable use
+       use w3schools for assistance
+       */
+        
+      $Val=101; // you forgot the semicolon here, statements are terminated using semicolon as we are used to do in C. But in Python semicolon is optional
+          echo "Value stored is " . $Val;
+          
+      ?>
+    
+
     </body>
 </html>
