1617d89f4a09269e3b7ded6d50f2bee4f77676b6
logan99 <nitish.courses@gmail.com>
2014-06-02 16:06:38 +0000

    practice

---
+++ index.php
@@ -0,0 +1,12 @@
+<!Doctype HTML>
+
+<html>
+  <head></head>
+  
+  <body>
+    <?php
+    echo "Hello prerna";
+    echo "<br/> Let's collobrate and build something nice!";
+?>
+    </body>
+</html>
